99809f52269af5624e9447af790377c064434c7e
Maxwell Roberts <maxwell.k.roberts@gmail.com>
2020-05-27 13:03:59 -0400

    wrote some axios requested, getting SQL access denied error

--- public/assets/javascript/javascript.js
+++ public/assets/javascript/javascript.js
@@ -1,17 +1,7 @@
+axios.get('/api/burger',data => console.log(data))
+
 document.getElementById('submitBtn').addEventListener('click',(event) => {
   event.preventDefault()
-  let newBurger = document.getElementById('newBurger').value
-  let newDiv = document.createElement('div')
-  newDiv.setAttribute('class','m-3 p-3 bg-info border rounded')
-  let newName = document.createElement('p')
-  let newButton = document.createElement('button')
-  newButton.setAttribute('id', newBurger)
-  newButton.textContent = "Devour"
-  newButton.addEventListener('click',(event) => {
-    event.preventDefault()
-  })
-  newName.textContent = newBurger
-  newDiv.append(newName)
-  newDiv.append(newButton)
-  document.getElementById('burgers').append(newDiv)
+  let newBurger = $('#newBurger').val()
+  axios.post('/api/burger',{name: newBurger, devoured: false})
 })
