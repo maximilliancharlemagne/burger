8e4496e5517534ad5b313f2d75339d7595910862
Maxwell Roberts <maxwell.k.roberts@gmail.com>
2020-05-27 09:58:49 -0400

    added some handlebars files, set up server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-require('express')
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
+app.listen(3000);
